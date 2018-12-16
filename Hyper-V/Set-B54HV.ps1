7eabfaf91967599d8ff5ae2d1d32428a0bb32e4d
unknown <kaddouriyasmine@hotmail.fr>
2018-12-16 17:22:25 -0500

    fix vm generation

--- Hyper-V/Set-B54HV.ps1
+++ Hyper-V/Set-B54HV.ps1
@@ -40,17 +40,17 @@ $virtualdisks = foreach ($disk in $virtualdiskname) {
 }
 #region Page 7, point 3
 $RAM = @(4096MB, 4096MB, 8192MB) # Ram Config (Start,Min,Max)
-$vm1 = New-VM -Name "Serveur1" -MemoryStartupBytes $RAM[0] -SwitchName "ComPublic" -VHDPath $virtualdisks[0].Path
+$vm1 = New-VM -Name "Serveur1" -MemoryStartupBytes $RAM[0] -SwitchName "ComPublic" -VHDPath $virtualdisks[0].Path -Generation 2
 $vm1 | set-vm -DynamicMemory -MemoryMinimumBytes $RAM[1] -MemoryMaximumBytes $RAM[2]
 $vm1 | Set-VMProcessor -Count 8
 # TODO: Écrire pour serveur 2 et 3
 
-$vm2 = New-VM -Name "Serveur2" -MemoryStartupBytes $RAM[0] -SwitchName "ComPublic" -VHDPath $virtualdisks[1].Path
+$vm2 = New-VM -Name "Serveur2" -MemoryStartupBytes $RAM[0] -SwitchName "ComPublic" -VHDPath $virtualdisks[1].Path -Generation 2
 $vm2 | set-vm -DynamicMemory -MemoryMinimumBytes $RAM[1] -MemoryMaximumBytes $RAM[2]
 Add-VMNetworkAdapter -VMName $vm2.VMName -SwitchName "ComPrive"
 $vm2 | Set-VMProcessor -Count 8
 
-$vm3 = New-VM -Name "Serveur3" -MemoryStartupBytes $RAM[0] -SwitchName "ComPrive" -VHDPath $virtualdisks[2].Path
+$vm3 = New-VM -Name "Serveur3" -MemoryStartupBytes $RAM[0] -SwitchName "ComPrive" -VHDPath $virtualdisks[2].Path -Generation 2
 $vm3 | set-vm -DynamicMemory -MemoryMinimumBytes $RAM[1] -MemoryMaximumBytes $RAM[2]
 $vm3 | Set-VMProcessor -Count 8
 
