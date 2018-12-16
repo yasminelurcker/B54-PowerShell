9ce405afb3a8bda5a1dc8285d2f638652b36a60a
unknown <kaddouriyasmine@hotmail.fr>
2018-12-16 18:29:54 -0500

    fix carte privé serveur 2

--- ConfigServeurs/Set-Serveur2.ps1
+++ ConfigServeurs/Set-Serveur2.ps1
@@ -8,9 +8,9 @@ New-NetIPAddress -IPAddress "10.57.54.104" -InterfaceIndex $nicPub.ifIndex -Pref
 Set-DnsClientServerAddress -InterfaceIndex $nicPub.ifIndex -ServerAddresses "127.0.0.1"
 New-NetFirewallRule -Name Allow_Ping -DisplayName “Allow Ping”  -Description “Packet Internet Groper ICMPv4” -Protocol ICMPv4 -IcmpType 8 -Enabled True -Profile Any -Action Allow
 
-$nicPrive = Get-NetAdapter | Where-Object { $_.ifIndex -ne $nicV1.ifIndex  }
-Rename-NetAdapter -Name $nicPrive.ifAlias -NewName "cartePrive"
-New-NetIPAddress -IPAddress "172.16.54.104" -InterfaceAlias $nicPrive.ifAlias -DefaultGateway "10.57.54.104" -PrefixLength 16
+$nicPrive = Get-NetAdapter | Where-Object { $_.ifIndex -ne $nicPub.ifIndex  }
+$nicPrive = Rename-NetAdapter -Name $nicPrive.ifAlias -NewName "cartePrive" -PassThru
+New-NetIPAddress -IPAddress "172.16.54.104" -InterfaceAlias $nicPrive.ifAlias -PrefixLength 16
 
 net use R: \\10.57.54.100\DATA
 #endregion
